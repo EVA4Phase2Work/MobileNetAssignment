d8987197f95b77f147e8dd4d851f52d1e96cd084
Monimoy Deb Purkayastha <monimoyd@gmail.com>
2020-08-09 22:40:45 +0530

    Add files via upload

---
+++ data_transformations/mobilenet_data_transformation.py
@@ -0,0 +1,24 @@
+
+import torchvision.transforms as transforms
+import numpy as np
+import torch
+
+import torchvision.transforms as transforms
+def get_train_transform():
+    transform = transforms.Compose(
+        [
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.ToTensor()])
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), ((0.2023, 0.1994, 0.2010)))])
+    return transform
+
+def get_test_transform():
+    transform = transforms.Compose(
+        [
+        transforms.Resize(224),
+        transforms.RandomCrop(224),
+        #transforms.Resize(224),
+        transforms.ToTensor()])
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), ((0.2023, 0.1994, 0.2010)))])
+    return transform
